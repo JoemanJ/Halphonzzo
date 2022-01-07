82cd3be573d8bbc3e686262f3164ad20fd2895c2
JoemanJ <diogodelazare@hotmail.com>
2022-01-07 12:45:31 -0300

    setup inicial

---
+++ src/scenes/level1.js
@@ -0,0 +1,15 @@
+import Phaser from '../lib/phaser.js'
+
+export default class level1 extends Phaser.Scene{
+    constructor(){
+        super('level1');
+    }
+    
+    preload(){
+
+    }
+
+    create(){
+
+    }
+}
