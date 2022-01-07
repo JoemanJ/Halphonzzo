2fc11b260ed8314378f466585f90ed47da3bf634
JoemanJ <diogodelazare@hotmail.com>
2022-01-07 14:13:57 -0300

    teste de fisica

--- src/main.js
+++ src/main.js
@@ -6,5 +6,14 @@ export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: level1
+    scene: level1,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 1000
+            },
+            debug: true
+        }
+    }
 })
--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -5,11 +5,41 @@ export default class level1 extends Phaser.Scene{
         super('level1');
     }
     
+    
     preload(){
-
+        this.load.image('halphonzzo', './src/sprites/halphonzzo.png');
+        this.load.image('chao', './src/sprites/chao.png');
     }
-
+    
     create(){
+        this.player = this.physics.add.image(16,500,'halphonzzo');
+        
+        const platforms = this.physics.add.staticGroup();
+        
+        const ground = platforms.create(160, 584, 'chao');
+        const body = ground.body
+        body.updateFromGameObject();
+        
+        this.physics.add.collider(this.player, platforms);
+        
+        this.keys = this.input.keyboard.createCursorKeys()
+    }
+    
+    update(){
+        if (this.keys.up.isDown && this.player.body.touching.down){
+            this.player.setVelocityY(-500);
+        }
 
+        if (this.keys.right.isDown){
+            this.player.setVelocityX(125);
+        }
+
+        else if(this.keys.left.isDown){
+            this.player.setVelocityX(-125)
+        }
+
+        else{
+            this.player.setVelocityX(0);
+        }
     }
 }
