dc6fc873fddcdc9a3099e8dfa48b326e03279240
JoemanJ <diogodelazare@hotmail.com>
2022-01-08 12:53:11 -0300

    mecanica de gravidade inicial

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -20,31 +20,38 @@ export default class level1 extends Phaser.Scene{
         const platforms = this.physics.add.staticGroup();
         
         //Adiciona o chao no grupo das plataformas
-        const ground = platforms.create(160, 584, 'chao');
-        const body = ground.body
-        body.updateFromGameObject();
+        platforms.create(160, 584, 'chao');
+        platforms.create(160, 16, 'chao');
         
         //adiciona colisao entre o player e as plataformas
         this.physics.add.collider(this.player, platforms);
         
         //cria o controle de inputs
         this.keys = this.input.keyboard.createCursorKeys()
+
+        //flag de mudança de gravidade
+        this.gravFlag = 1;
+        this.physics.add.collider(this.player, platforms, function (){this.gravflag = 1});
     }
     
     update(){
 
         //MOVIMENTO
         //pulo
-        if (this.keys.up.isDown && this.player.body.touching.down){
-            this.player.setVelocityY(-500);
+        if (this.keys.up.isDown && //cima apertado E...
+            (
+                (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y)) || //gravidade pra baixo e player tocando o chao OU...
+                (this.player.body.touching.up && -1 * Math.sign(this.physics.world.gravity.y)) //gravidade pra cima e player tocando o teto
+            )){
+            this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //muda a velocidade do player no sentido contrário à gravidade
         }
 
-        //esquerda
+        //direita
         if (this.keys.right.isDown){
             this.player.setVelocityX(125);
         }
 
-        //direita
+        //esquerda
         else if(this.keys.left.isDown){
             this.player.setVelocityX(-125)
         }
@@ -52,6 +59,12 @@ export default class level1 extends Phaser.Scene{
         //parado
         else{
             this.player.setVelocityX(0);
+        }
+
+        //Mudar a gravidade
+        if (this.keys.space.isDown && this.gravFlag == 1){
+            this.physics.world.gravity.y *= -1;
+            this.gravFlag = 0;
         }
     }
 }
