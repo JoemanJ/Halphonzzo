00aa827bf7e8d3f487402ddc90a82ffa418f2b83
JoemanJ <diogodelazare@hotmail.com>
2022-01-09 18:56:22 -0300

    movimento e gravidade consertados

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -17,36 +17,47 @@ export default class level1 extends Phaser.Scene{
         this.player = this.physics.add.image(16,500,'halphonzzo');
         
         //cria o grupo das plataformas
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
         
         //Adiciona o chao no grupo das plataformas
-        platforms.create(160, 584, 'chao');
-        platforms.create(160, 16, 'chao');
-        platforms.create(320, 380, 'chao');
+        this.platforms.create(160, 584, 'chao');
+        this.platforms.create(160, 16, 'chao');
+        this.platforms.create(320, 380, 'chao');
         
         //adiciona colisao entre o player e as plataformas
-        this.physics.add.collider(this.player, platforms);
+        this.physics.add.collider(this.player, this.platforms);
         
         //cria o controle de inputs
         this.keys = this.input.keyboard.createCursorKeys()
 
         //flag de mudança de gravidade
-        this.gravFlag = 1;
-        this.physics.add.collider(this.player, platforms, function (){this.gravflag = 1});
+        this.gravFlag = 0;
     }
     
     update(){
 
         //MOVIMENTO
-        //pulo
-        if (this.keys.up.isDown && //cima apertado E...
-            (
-                (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
-                (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
-            )
-            ){
-            this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //muda a velocidade do player no sentido contrário à gravidade
+        //flag de gravidade e pulo
+        if(
+            (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
+            (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
+        ){
+            this.gravFlag = 1; //Reseta o flag de gravidade
+            
+            if(this.keys.up.isDown){ //Cima apertado
+                this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //Pulo
+                console.log("teste");
+            }
         }
+
+        // if (this.keys.up.isDown && //cima apertado E...
+        //     (
+        //         (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
+        //         (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
+        //     )
+        //     ){
+        //     this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //muda a velocidade do player no sentido contrário à gravidade
+        // }
 
         //direita
         if (this.keys.right.isDown){
@@ -71,5 +82,10 @@ export default class level1 extends Phaser.Scene{
             this.player.flipY = !this.player.flipY;
             this.gravFlag = 0;
         }
+    }
+
+    setGravFlag(){
+        this.gravFlag = 1;
+        console.log("teste");
     }
 }
