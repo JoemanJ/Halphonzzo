6e8758a91ff7162b98e56426bb212457c899fd9e
JoemanJ <diogodelazare@hotmail.com>
2022-01-11 15:30:49 -0300

    mecânicas prontas

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -5,7 +5,8 @@ export default class level1 extends Phaser.Scene{
         super('level1');
     }
     
-    
+    debugMode = false;
+
     preload(){
         //Carrega as imagens
         this.load.image('halphonzzo', './src/sprites/halphonzzo.png');
@@ -26,11 +27,12 @@ export default class level1 extends Phaser.Scene{
         this.platforms = this.physics.add.staticGroup();
         
         //Cria o grupo dos inimigos
-        this.tomatos = this.physics.add.staticGroup();
+        this.enemies = this.add.group();
 
-        const tomato = this.add.image(100, 500, 'tomato');
-        this.tomatos.add(tomato);
-        
+        //Cria o tomato voador
+        const tomato = this.physics.add.image(200, 320, 'tomato');
+        tomato.body.setAllowGravity(false);
+        this.enemies.add(tomato);
 
         //cria o chão e o teto como tilesprites e adiciona ao grupo das pĺataformas
         const teto = this.add.tileSprite(400, 16, 800, 32, 'chao');
@@ -52,7 +54,7 @@ export default class level1 extends Phaser.Scene{
         //adiciona colisao entre o player e as plataformas
         this.collider_player_platforms = this.physics.add.collider(this.player, this.platforms);
         
-        this.collider_player_tomatos = this.physics.add.collider(this.player, this.tomatos, this.killHalphonzzo, undefined, this);
+        this.collider_player_enemies = this.physics.add.overlap(this.player, this.enemies, this.handleHit, undefined, this);
 
 
 
@@ -68,8 +70,9 @@ export default class level1 extends Phaser.Scene{
         this.keys = this.input.keyboard.createCursorKeys()
 
         //flag de mudança de gravidade
-        this.gravFlag = 0;
+        this.gravFlag = false;
 
+        //Configuração da câmera
         this.cameras.main.startFollow(this.player)
         this.cameras.main.setFollowOffset(0, 200)
         this.cameras.main.setBounds(0, 0, 1000, 600)
@@ -78,65 +81,128 @@ export default class level1 extends Phaser.Scene{
     
     update(){
 
+        //Movimentação do jogador
+        this.handlePlayerMovement();
+
+        //Movimentação dos inimigos
+        this.handleEnemyMovement(this.time.now);
+    }
+
+    handlePlayerMovement(){
         //MOVIMENTO
-        //flag de gravidade e pulo
-        if(
-            (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
-            (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
-        ){
-            this.gravFlag = 1; //Reseta o flag de gravidade
-            
-            if(this.keys.up.isDown){ //Cima apertado
-                this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //Pulo
-                console.log("teste");
+        if(!this.debugMode){
+            //flag de gravidade e pulo
+            if(
+                (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
+                (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
+            ){
+                this.gravFlag = true; //Reseta o flag de gravidade
+                
+                if(this.keys.up.isDown){ //Cima apertado
+                    this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //Pulo
+                }
+            }
+
+            //direita
+            if (this.keys.right.isDown){
+                this.player.setVelocityX(125);
+                this.player.flipX = false;
+            }
+
+            //esquerda
+            else if(this.keys.left.isDown){
+                this.player.setVelocityX(-125)
+                this.player.flipX = true;
+            }
+
+            //parado
+            else{
+                this.player.setVelocityX(0);
+            }
+
+            //Mudar a gravidade
+            if (this.keys.space.isDown && this.gravFlag){
+                this.physics.world.gravity.y *= -1;
+                this.player.flipY = !this.player.flipY;
+                this.gravFlag = false;
+            }
+
+        }
+
+        else{ //Modo debug provisório
+            if(this.keys.up.isDown){
+                this.player.setVelocityY(-100);
+            }
+            else if(this.keys.down.isDown){
+                this.player.setVelocityY(100);
+            }
+            else if(this.keys.left.isDown){
+                this.player.setVelocityX(-100);
+            }
+            else if(this.keys.right.isDown){
+                this.player.setVelocityX(100);
+            }
+
+            else{
+                this.player.setVelocityX(0);
+                this.player.setVelocityY(0);
             }
         }
 
-        // CODIGO DE PULO ANTERIOR (não resetava o flag de gravidade)
-        // if (this.keys.up.isDown && //cima apertado E...
-        //     (
-        //         (this.player.body.touching.down && Math.sign(this.physics.world.gravity.y) == 1) || //gravidade pra baixo e player tocando o chao OU...
-        //         (this.player.body.touching.up && Math.sign(this.physics.world.gravity.y) == -1) //gravidade pra cima e player tocando o teto
-        //     )
-        //     ){
-        //     this.player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //muda a velocidade do player no sentido contrário à gravidade
-        // }
-
-        //direita
-        if (this.keys.right.isDown){
-            this.player.setVelocityX(125);
-            this.player.flipX = false;
-        }
-
-        //esquerda
-        else if(this.keys.left.isDown){
-            this.player.setVelocityX(-125)
-            this.player.flipX = true;
-        }
-
-        //parado
-        else{
-            this.player.setVelocityX(0);
-        }
-
-        //Mudar a gravidade
-        if (this.keys.space.isDown && this.gravFlag == 1){
-            this.physics.world.gravity.y *= -1;
-            this.player.flipY = !this.player.flipY;
-            this.gravFlag = 0;
+        if(this.keys.shift.isDown){ //Ativação do modo debug **TROCAR PARA KEY COMBO**
+            this.debugMode = !this.debugMode;
+            this.player.body.setAllowGravity(!this.debugMode);
         }
     }
 
-    //ANIMAÇÃO DE MORTE
-    killHalphonzzo(){
-        this.physics.world.gravity.y = 1000;
+    //MOVIMENTO DOS INIMIGOS
+    handleEnemyMovement(time){
+        //Faz os inimigos andarem em zigzag
+        this.enemies.getChildren().forEach(function(enemy){
+            if(time%4000 < 2000){
+                enemy.setVelocityX(75);
+                enemy.flipX = true;
+            }
+            else{
+                enemy.setVelocityX(-75);
+                enemy.flipX = false;
+            }
+        })
+    }
+
+    handleHit(player, enemy){
+        if(
+            ( (player.body.y + 54) > enemy.body.y && Math.sign(this.physics.world.gravity.y) == 1) || //inimigo acima e gravidade para baixo
+            ( (player.body.y + 10) < (enemy.body.y + enemy.body.height) && Math.sign(this.physics.world.gravity.y) == -1) //Inimigo abaixo e gravidade para cima
+            ){
+                //ANIMAÇÃO DE MORTE
+                this.killPlayer();
+            }
+
+            else{
+                //Animação de morte do inimigo
+            this.enemies.remove(enemy); //Tira colisão do inimigo
+            enemy.flipY = true; // Gira o inimigo
+            enemy.setVelocityX(0); //Pulinho
+            enemy.setVelocityY(-200);
+            enemy.setAccelerationY(1000);
+
+            player.setVelocityY(Math.sign(this.physics.world.gravity.y) * -500); //Pulo do player
+            this.gravFlag = true //Reset do flag de gravidade
+        }
+    }
+
+    killPlayer(){
+        this.input.keyboard.destroy() //Não pode se mover após a morte
+
         this.player.flipY = false;
         this.player.setAngle(180);
+
+        this.collider_player_platforms.destroy(); // Não colide com nada após a morte
+        this.collider_player_enemies.destroy(); 
         this.player.setCollideWorldBounds(false);
-        this.player.setVelocityY(-500);
-        this.physics.world.removeCollider(this.collider_player_platforms);
-        this.physics.world.removeCollider(this.collider_player_tomatos);
-        this.input.keyboard.destroy()
+
+        this.player.setVelocityY(-500); //Cai para baixo independente do sentido da gravidade
     }
 
 }
